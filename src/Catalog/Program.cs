eb9595d4c7e2d096088982cefa30849ffdcb5aaf
cemkilinc0 <cemkilinc0@gmail.com>
2024-02-09 16:27:19 +0300

    Services moved under src folder

---
+++ src/Catalog/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using Catalog.Repositories; 
+using Catalog.Data;
+using Catalog.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CatalogConnection")));
+
+builder.Services.AddScoped<IGameItemRepository, GameItemRepository>();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<CatalogDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("-------" + ex);
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseHttpsRedirection();
+app.UseRouting(); 
+app.MapControllers(); 
+
+app.Run();
+
